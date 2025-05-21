27d04da34285d7383f934fe0046897bde779ba6e
Youssef-YN <nazihyoussef05@gmail.com>
2025-05-21 09:59:56 +0100

    Add files via upload

--- autoservmaker.sh
+++ autoservmaker.sh
@@ -1,3 +1,10 @@
+copy_source="cp_source_paths.as"
+copy_dest="cp_dest_paths.as"
+mv_source="mv_source_paths.as"
+mv_dest="mv_dest_paths.as"
+del_path="delete_paths.as"
+script_path="script_paths"
+
 option_one() {
     clear
     read -e -p "Source > " source_path
@@ -6,9 +13,9 @@ option_one() {
         read -e -p "Destination > " dest_path
         
         if [[ -n "$dest_path" ]]; then
-            echo "$source_path" >> cp_source_paths
-            echo "$dest_path" >> cp_destination_paths            
-            echo "Paths saved to cp_source_paths and cp_destination_paths"
+            echo "$source_path" >> $copy_source
+            echo "$dest_path" >> $copy_dest            
+            echo "Paths saved to $copy_source and $copy_dest"
         else
             echo "No destination path entered."
         fi
@@ -25,10 +32,10 @@ option_two() {
         read -e -p "Destination > " dest_path
         
         if [[ -n "$dest_path" ]]; then
-            echo "$source_path" >> mv_source_paths.txt
-            echo "$dest_path" >> mv_destination_paths.txt
+            echo "$source_path" >> $mv_source
+            echo "$dest_path" >> $mv_dest
             
-            echo "Paths saved to mv_source_paths and mv_destination_paths"
+            echo "Paths saved to $mv_source and $mv_dest"
         else
             echo "No destination path entered."
         fi
@@ -41,8 +48,8 @@ option_three() {
     clear
     read -e -p "File path to delete > " file_to_delete
     if [[ -n "$file_to_delete" ]]; then
-	echo "$file_to_delete" >> delete_paths
-	echo "File saved to delete delete_paths"
+	echo "$file_to_delete" >> $del_path
+	echo "File saved to delete $del_path"
     else
 	echo "No paths entered"
     fi
@@ -52,21 +59,53 @@ option_four() {
     clear
     read -e -p "Scripts to execute > " script_to_run
     if [[ -n "$script_to_run" ]]; then
-	echo "$script_to_run" >> script_paths
+	echo "$script_to_run" >> $script_path
 	echo "File saved to delete scripts"
     else
 	echo "No scripts entered"
     fi
-    }
+}
+
+option_five() {
+    clear
+    if [ -f "./$copy_source" ]; then
+	less "./$copy_source"
+	less "./$copy_dest"
+    else
+	echo "No copy paths"
+    fi
+
+
+    if [ -f "./$mv_source" ]; then
+	less "./$mv_source"
+	less "./$mv_dest"
+    else
+	echo "No move paths"
+    fi
+
+     if [ -f "./$del_path" ]; then
+	less "./$del_path"
+    else
+	echo "No delete paths"
+    fi
+
+     if [ -f "./$script_path" ]; then
+	less "./$script_path"
+    else
+	echo "No script paths"
+    fi
+}
 
 # Show the menu
 show_menu() {
     echo "Select an option:"
-    echo "1) Copy Files & Directories"
-    echo "2) Move Files & Directories"
-    echo "3) Delete Files & Directories"
+    echo "1) Add files to copy"
+    echo "2) Add files to move"
+    echo "3) Add files to remove"
     echo "4) Run Scripts"
-    echo "5) Exit"
+    echo "5) Status"
+    echo "6) Create autoserv.sh"
+    echo "7) Exit"
 }
 
 # Main loop
@@ -78,7 +117,9 @@ while true; do
         2) option_two ;;
         3) option_three ;;
         4) option_four ;;
-        5) echo "Exiting..."; break ;;
+	5) option_five ;;
+	6) option_six ;;
+        7) echo "Exiting..."; break ;;
         *) echo "Invalid option, try again." ;;
     esac
     echo ""
